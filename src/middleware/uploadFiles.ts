86cb938da5140d3ba7bd925d3f9233d5539746ed
Ibrahim-hasanean <imh19970408@gmail.com>
2024-11-05 21:47:55 +0200

    check if directory exist when upload image

--- src/middleware/uploadFiles.ts
+++ src/middleware/uploadFiles.ts
@@ -1,9 +1,15 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 import ApiError from "../utils/ApiError";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "../public/images"));
+    const uploadDirectory = path.join(__dirname, "../public/images");
+    if (!fs.existsSync(uploadDirectory)) {
+      // If not, create it
+      fs.mkdirSync(uploadDirectory, { recursive: true });
+    }
+    cb(null, uploadDirectory);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
